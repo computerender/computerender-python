21f784dee350dfd9c973861f2406b0237b65064a
PWhiddy <all.cows.like.to.moo@gmail.com>
2022-12-01 01:08:01 -0800

    starting to test requests

--- src/computerender/client.py
+++ src/computerender/client.py
@@ -1,14 +1,31 @@
 """computerender."""
-
+from typing import Optional, Dict, Any
+import aiohttp
+from aiohttp import FormData
+import os
+from urllib.parse import quote_plus
 
 class Computerender:
     """Client for the computerender API."""
 
     api_key: str
+    base_url = "https://api.computerender.com"
 
-    def __init__(self, api_key: str = "bad") -> None:
+    def __init__(self, api_key: Optional[str] = None) -> None:
         """Initialize api client."""
-        self.api_key = api_key
+        self.api_key = api_key or os.environ["CR_KEY"]
+
+    async def generate(self, prompt: str, **kwargs: Dict[str, Any]) -> bytes:
+        """Generate an image."""
+        method = "generate/"
+        form_data = FormData(kwargs)
+        async with aiohttp.ClientSession.request() as session:
+            result = await session.post(
+                self.base_url + method,
+                data=form_data,
+                headers={"Authorization": f"X-API-Key {self.api_key}"}
+            )
+            return result
 
     def do_it(self) -> None:
         """Test method that does nothing."""
--- tests/test_client.py
+++ tests/test_client.py
@@ -1,6 +1,6 @@
 """Test cases for the client module."""
 from computerender import Computerender
-
+import pytest
 
 def test_client_succeeds_no_key() -> None:
     """Client can be initialized with no arg."""
@@ -11,7 +11,7 @@ def test_client_succeeds_no_key() -> None:
 def test_client_succeeds_w_key() -> None:
     """Client can be initialized with key arg."""
     cr_client = Computerender("test")
-    assert cr_client is not None
+    assert cr_client.api_key == "test"
 
 
 def test_client_succeeds_test() -> None:
@@ -19,3 +19,10 @@ def test_client_succeeds_test() -> None:
     cr_client = Computerender()
     cr_client.do_it()
     assert cr_client is not None
+
+@pytest.mark.asyncio
+async def test_client_generate_basic() -> None:
+    """Client returns data on generate"""
+    cr_client = Computerender()
+    result = await cr_client.generate("what's up gamers", iterations=20)
+    assert result is not None
